88733dbb4fb00908f53fc0a1621a5c5153731243
WalidDev91 <walid.boulima.dev@outlook.com>
2026-04-16 18:41:21 +0100

    IgnoreJSON

--- src/main/java/com/mvpnest/fleetmanagement/entity/Vehicle.java
+++ src/main/java/com/mvpnest/fleetmanagement/entity/Vehicle.java
@@ -1,5 +1,6 @@
 package com.mvpnest.fleetmanagement.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -37,16 +38,19 @@ public class Vehicle extends BaseEntity {
     // 1/ Vehicle (*) → User (1) "admin"
     @ManyToOne
     @JoinColumn(name = "admin_id")
+    @JsonIgnore
     private User admin;
     // fix: each vehicle is managed by one admin
 
     // 2/ Vehicle (1) → Mission (*)
     @OneToMany(mappedBy = "vehicle")
+    @JsonIgnore
     private List<Mission> missions;
     // fix: one vehicle can be used in multiple missions
 
     // 3/ Vehicle (1) → VehicleDocument (*)
     @OneToMany(mappedBy = "vehicle")
+    @JsonIgnore
     private List<VehicleDocument> vehicleDocuments;
     // fix: one vehicle has multiple documents
 }
